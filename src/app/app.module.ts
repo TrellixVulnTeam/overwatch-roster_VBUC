17f513cf3d335f1a7584b9bfb724c6d4ffd2852e
Christian Martinez <cgmartinez11142@gmail.com>
2017-05-12 13:54:28 -0700

    add necessary components, pipes, and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+import { AdminComponent } from './admin/admin.component';
+import { EditComponent } from './edit/edit.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { RolePipe } from './role.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterListComponent,
+    CharacterDetailComponent,
+    AdminComponent,
+    EditComponent,
+    WelcomeComponent,
+    AboutComponent,
+    RolePipe
   ],
   imports: [
     BrowserModule,

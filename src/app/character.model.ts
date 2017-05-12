b8323fcc7769bbd6a554b15d3023e6f80732c4c9
Christian Martinez <cgmartinez11142@gmail.com>
2017-05-12 15:07:16 -0700

    link firebase

--- src/app/character.model.ts
+++ src/app/character.model.ts
@@ -1,2 +1,3 @@
 export class Character {
+  constructor(public name: string, public role: string, public dificulty: number, public health: number, public armor: number, public shield: number, public total: number, public place: string) { }
 }

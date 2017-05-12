5630d21731c99cc8a419440e242cbbf77501d3fc
Christian Martinez <cgmartinez11142@gmail.com>
2017-05-12 15:39:26 -0700

    add sample characters in characters.json

--- src/app/character.model.ts
+++ src/app/character.model.ts
@@ -1,3 +1,3 @@
 export class Character {
-  constructor(public name: string, public role: string, public dificulty: number, public health: number, public armor: number, public shield: number, public total: number, public place: string) { }
+  constructor(public imageUrl: string, public name: string, public role: string, public difficulty: number, public health: number, public armor: number, public shield: number, public total: number, public place: string) { }
 }

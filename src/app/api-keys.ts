6bb61d1ea9f2c2ffd084443b4aeffa36da8c1d85
Christian Martinez <cgmartinez11142@gmail.com>
2017-10-12 12:26:24 -0700

    add api.ts

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyCcJVRwYpC96rLa_Aka12o_XO6k-VycbkY",
+    authDomain: "overwatch-roster.firebaseapp.com",
+    databaseURL: "https://overwatch-roster.firebaseio.com",
+    projectId: "overwatch-roster",
+    storageBucket: "overwatch-roster.appspot.com",
+    messagingSenderId: "540813739819"
+  };
